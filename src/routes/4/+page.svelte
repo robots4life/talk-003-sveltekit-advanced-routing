41385580c117588da41171446d47ee7a2dd4476b
robots4life <34193243+robots4life@users.noreply.github.com>
2023-08-22 17:42:05 +0200

    slide-4

---
+++ src/routes/4/+page.svelte
@@ -0,0 +1,19 @@
+<h1 class="pt-20 font-bold">Route parameters</h1>
+
+<a
+	class="block text-3xl"
+	target="_blank"
+	rel="noreferrer"
+	href="https://learn.svelte.dev/tutorial/params">https://learn.svelte.dev/tutorial/params</a
+>
+
+<p>
+	To create routes with dynamic parameters, use square brackets around a valid variable name as the
+	folder name.
+</p>
+
+<pre class="pt-12 text-2xl">[projectID]</pre>
+
+<pre class="pt-12 text-2xl">routes/work/[projectID]</pre>
+
+<pre class="pt-12 text-2xl">git checkout slide-4</pre>
